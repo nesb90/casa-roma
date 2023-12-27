5912f17f89962e981b86cc42686fe10a1a1575b2
Nestor Soto <nsoto@foundationmedicine.com>
2023-12-27 16:59:47 -0600

    remove unnecessary function

--- src/utils/handler.utils.js
+++ src/utils/handler.utils.js
@@ -55,47 +55,6 @@ function select (tableName, id, props = []) {
   };
 };
 
-function selectOrders (tableName, id, props = [], filters = {}) {
-  let query =  `select * from ${POSTGRES.SCHEMA}.${tableName}`;
-
-  if (props.length > 0) {
-    const keys = props.map((prop) => {
-      return _.snakeCase(prop);
-    });
-    query = query.replace('*', `${keys.join(',')}`);
-  };
-
-  if (!_.isEmpty(filters)) {
-    if (filters.startDate && filters.endDate) {
-      query = query.concat(` where created_at BETWEEN '${filters.startDate}' and '${filters.endDate}'`);
-    };
-
-    if (filters.cancelled) {
-      if (query.includes('where')) {
-        query = query.concat(` and is_cancelled = ${filters.cancelled}`);
-      } else {
-        query = query.concat(` where is_cancelled = ${filters.cancelled}`);
-      };
-    }
-
-    if (filters.completed) {
-      if (filters.cancelled) {
-        query = query.concat(' or returned_at notnull');
-      } else if (!filters.cancelled && query.includes('where')) {
-        query = query.concat(' and returned_at notnull');
-      } else {
-        query = query.concat(' where returned_at notnull');
-      }
-    };
-  };
-
-  if (id) {
-    return [`${query} where id=($1)`, [id]];
-  } else {
-    return `${query} order by id ASC`;
-  };
-};
-
 /**
  *
  * @param {String} tableName The table name.
@@ -144,7 +103,6 @@ module.exports = {
   queryBuilder: {
     insert,
     select,
-    selectOrders,
     update,
     remove
   }
